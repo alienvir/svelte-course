3fe5ebd07b0f2d3e81b8a8912a0a42175ed30f20
alienvir <roland@alienvir.us>
2020-03-06 21:46:36 -0600

    end of section 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,32 @@
 <script>
-  let name = "Max";
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "";
+  let jobTitle = "";
+  let description = "";
+  let userImage;
   let age = 30;
+
+  $: uppercaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if (name === "Maximilian") {
+    console.log("It runs!");
+    age = 31;
+  }
 
   function incrementAge() {
     age += 1;
+  }
+
+  function changeName() {
+    name = "Maximilian";
+  }
+
+  function nameInput(event) {
+    const enteredValue = event.target.value;
+    name = enteredValue;
   }
 </script>
 
@@ -13,5 +36,11 @@
   }
 </style>
 
-<h1>Hello {name}, my age is {age}!</h1>
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
+<input type="text" bind:value={name} />
+<input type="text" bind:value={jobTitle} />
+<input type="text" bind:value={description} />
+<input type="text" bind:value={userImage} />
+
+<ContactCard {name} {jobTitle} {description} {userImage} />
